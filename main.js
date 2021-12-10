bc3cbe504abb937ee1d764c9c3e2f91a358d993c
Evelyn-Adams <94699970+Evelyn-Adams@users.noreply.github.com>
2021-12-10 16:44:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ function preload(){
 }
 
 function setup(){
-canvas=createCanvas(2000,1000);
-canvas.position(100,0);
+canvas=createCanvas(700,600);
+canvas.center();
 video=createCapture(VIDEO);
 video.hide();
 }
 
 function draw(){
-image(video,640,480,300,400);
-    circle(550,400,100);
-    circle(550,975,100);
-    circle(1010,400,100);
-    circle(1010,975,100);
+image(video,300,100,300,400);
+    circle(300,100,100);
+    circle(600,100,100);
+    circle(600,500,100);
+    circle(300,500,100);
     stroke(280,0,100);
     fill(280,0,100);
     
-    rect(600, 375, 360, 55);
-    rect(600, 950, 360, 55);
-    rect(520, 445, 55, 485);
-    rect(982, 445, 55, 485);
+    rect(350, 75, 200, 55);
+    rect(350, 475, 200, 55);
+    rect(275, 150, 55, 300);
+    rect(575, 150, 55, 300);
     fill(0,0,280);
 }
 
